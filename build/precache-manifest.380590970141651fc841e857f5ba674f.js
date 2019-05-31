83256fb1de77e9ae9467adb8698b47ccfb66fa62
sulemanmalik <sulemanmalik@Sulemans-MacBook-Pro.local>
2019-05-31 17:13:04 -0400

    production build

---
+++ build/precache-manifest.380590970141651fc841e857f5ba674f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561700beb247dcbc71f12f11416de235",
+    "url": "/index.html"
+  },
+  {
+    "revision": "630cd3d4932f2d669c06",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e16a3123ff7775e402b2",
+    "url": "/static/js/2.9a0b0384.chunk.js"
+  },
+  {
+    "revision": "630cd3d4932f2d669c06",
+    "url": "/static/js/main.dde7a2d1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78393680a9dc32027a91d205e146b80f",
+    "url": "/static/media/Defilia.78393680.jpg"
+  },
+  {
+    "revision": "81f86731bcb5588912e6c4ee23b9f2ff",
+    "url": "/static/media/Dylan.81f86731.jpg"
+  },
+  {
+    "revision": "b14baa661512aad6440964745fad3a46",
+    "url": "/static/media/Greg.b14baa66.jpg"
+  },
+  {
+    "revision": "727c7aa9c5fe86a63be6aad6ea25d055",
+    "url": "/static/media/Hansen.727c7aa9.jpg"
+  },
+  {
+    "revision": "ec926eef633b0c2941a6a5333bbc4541",
+    "url": "/static/media/Itai.ec926eef.jpg"
+  },
+  {
+    "revision": "1b4ddcd6efc39c437df5e9500f65f543",
+    "url": "/static/media/Judy.1b4ddcd6.jpg"
+  },
+  {
+    "revision": "8c18d4ce5eef8edfb8ad6538002056fb",
+    "url": "/static/media/Logo-Text.8c18d4ce.png"
+  },
+  {
+    "revision": "bc88f9c25f8465c1caff286aa4d0d261",
+    "url": "/static/media/Nabila.bc88f9c2.jpg"
+  },
+  {
+    "revision": "28e45ff94329db9aafa7e67d76c7116b",
+    "url": "/static/media/Salar.28e45ff9.jpg"
+  },
+  {
+    "revision": "4df32fc435279cfa4660578976f27662",
+    "url": "/static/media/Sinan.4df32fc4.jpg"
+  },
+  {
+    "revision": "50f85e3e17c0797d8fa971052243ad08",
+    "url": "/static/media/Winter_2019_V2_2.50f85e3e.png"
+  },
+  {
+    "revision": "228afd7147f835b318b1978d5f4c9383",
+    "url": "/static/media/dawn.228afd71.jpg"
+  },
+  {
+    "revision": "8e3c83ba6154eaabf4d6792938583f65",
+    "url": "/static/media/epts.8e3c83ba.jpg"
+  },
+  {
+    "revision": "8649af831cc6d990b4c9c728a228ab63",
+    "url": "/static/media/facebook-cover.8649af83.png"
+  }
+]);
